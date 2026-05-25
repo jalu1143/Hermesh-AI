9a020677f867d71a6d3ac6792882b9d505cb14a6
jalu1143 <76772749+jalu1143@users.noreply.github.com>
2026-05-26 01:20:56 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,11 @@
-const CACHE_NAME = 'hermesh-ai-v1';
+const CACHE_NAME = 'hermesh-pro-v2';
 const ASSETS = [
   'index.html',
   'manifest.json'
 ];
-
-// Install Event
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-
-// Fetch Event (Offline support ke liye)
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
