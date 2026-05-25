8a5d5741e2b0cf3f7e4e4d7051815919d44aa91a
jalu1143 <76772749+jalu1143@users.noreply.github.com>
2026-05-25 22:39:10 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'hermesh-ai-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event (Offline support ke liye)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
